d9f43d6c95e1f83d7fc10872ef81217e50ce42fe
Joost <j.verbraeken@student.tudelft.nl>
2019-03-18 17:49:59 +0100

    More disps

--- FinalProject/FinalProject.m
+++ FinalProject/FinalProject.m
@@ -6,6 +6,7 @@ disp("0nd step: Read the images and resize");
 I = imageParser('model_castle', 'JPG');
 I = imresize(I, 0.5);  % Prevent Out-of-Memory exception
 
+disp("----");
 %% 1st step: Find correspondences between consecutive matching
 disp("1st step: Find correspondences between consecutive matching");
 
@@ -19,17 +20,25 @@ else
     [match1, match2] = findMatches(I(:,:,ind), I(:,:,ind+1), dist_thres, edge_thres, mode);
 end
 
+disp("----");
 %% 2nd step: Apply normalized 8-point RANSAC algorithm to find best matches
 disp("2nd step: Apply normalized 8-point RANSAC algorithm to find best matches");
+[F, inliers] = estimateFundamentalMatrix(X1,X2);
 
+disp("----");
 %% 3rd step: Represent point correspondes for different camera views
 disp("3rd step: Represent point correspondes for different camera views");
 
+disp("----");
 %% 4th step: Stitch points together
 disp("4th step: Stitch points together");
 
+disp("----");
 %% 5th step: Eliminate affine ambiguity
 disp("5th step: Eliminate affine ambiguity");
 
+disp("----");
 %% 6th step: Plot 3D model
 disp("6th step: Plot 3D model");
+
+disp("----");
